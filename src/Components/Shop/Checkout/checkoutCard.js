3477062859098286a88b9f7d9c44476f6377f98c
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-08-23 16:46:27 +0300

    ..

--- src/Components/Shop/Checkout/checkoutCard.js
+++ src/Components/Shop/Checkout/checkoutCard.js
@@ -6,11 +6,11 @@ import { FaLocationArrow } from 'react-icons/fa';
 import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import CheckoutSummary from '../CheckoutSummary/checkoutSummary';
-// import { pushOrders } from '../../../app/features/cartSlice';
+import { clearCart } from '../../../app/features/cartSlice';
 // import { useDispatch } from 'react-redux';
 
 function CheckoutCard() {
-  // const dispatch =useDispatch();
+  const dispatch =useDispatch();
   const { total ,cartItems} = useSelector((store) => store.cart);
   const taxPrice =total *0.14;
   const totalPrice =total + taxPrice;
@@ -23,6 +23,7 @@ function CheckoutCard() {
   async function onHandlePayment(e){
           e.preventDefault();
           setPhoneNo('');
+          dispatch(clearCart())
   const usersItems =[];
   const orders = usersItems.concat(cartItems,{'amount':amount,'phoneNo':phoneNo});
   console.log(orders)
